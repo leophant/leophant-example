1ad252b806b48fde4e669131135be50636500b9e
onwd <onwdit@gmail.com>
2019-01-26 13:50:32 +0600

    Pass the Connector itself instead of Connector name

--- src/config/datasources.js
+++ src/config/datasources.js
@@ -1,20 +1,23 @@
+const MySQLConnector      = require('leophant-mysql');
+const PostgreSQLConnector = require('leophant-postgresql');
+
 module.exports = [
   {
-    "name": "db1",
-    "connector": "mysql",
-    "host": "localhost",
-    "port": 5432,
-    "database": "db1",
-    "username": "username",
-    "password": "password"
+    name: 'db1',
+    connector: MySQLConnector,
+    host: 'localhost',
+    port: 5432,
+    database: 'db1',
+    username: 'username',
+    password: 'password'
   },
   {
-    "name": "db2",
-    "connector": "postgresql",
-    "host": "localhost",
-    "port": 5432,
-    "database": "db2",
-    "username": "username",
-    "password": "password"
+    name: 'db2',
+    connector: PostgreSQLConnector,
+    host: 'localhost',
+    port: 5432,
+    database: 'db2',
+    username: 'username',
+    password: 'password'
   }
 ];
