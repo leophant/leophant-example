a9e8f684b9a7c0ec4f532fd6496b6707f1f9c610
onwd <onwdit@gmail.com>
2019-01-28 23:21:08 +0600

    Revert "Update models.js"
    This reverts commit a5c5886d5c303b5c11bba321fafd87193e21725f.

--- src/config/models.js
+++ src/config/models.js
@@ -1,6 +1,6 @@
 // TODO: auto-discover models instead of defining them manually
-module.exports = {
-  File: require('../models/file.json'),
-  Role: require('../models/role.json'),
-  User: require('../models/user.json')
-};
+module.exports = [
+  require('../models/file.json'),
+  require('../models/role.json'),
+  require('../models/user.json')
+];
